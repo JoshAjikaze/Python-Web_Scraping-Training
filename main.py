6b5f82df472a775219dae53039e9024d660f2fee
Ajiwojuolorun Joshua Ayomide <ajiwojuolorunjoshua@gmail.com>
2024-05-07 21:47:08 +0100

    consolidation

---
+++ main.py
@@ -0,0 +1,29 @@
+# Import urlopen from request
+from urllib.request import urlopen
+
+# define your url
+url = "http://olympus.realpython.org/profiles/aphrodite"
+url2 = "http://olympus.realpython.org/profiles/poseidon"
+
+# open the webpage by passing it into urlopen
+# This returns an HTTPResponse object
+page = urlopen(url2)
+
+# Extract the raw html with the read method
+raw_html = page.read()
+
+# Decode the raw html 
+html = raw_html.decode("utf-8")
+
+# print the decodes html
+print(html)
+
+title_index = html.find("<title>")
+
+start_index = title_index + len("<title>")
+end_index = html.find("</title>")
+# print(start_index, end_index)
+
+web_page_title = html[start_index:end_index]
+print(web_page_title)
+
